e954bf7aa1cc6688d7114ba9fb5e1ff9bf41bed1
kellijohnson <kellifayejohnson@gmail.com>
2014-06-10 13:40:14 -0700

    Add akCRS to define_projections.R
    Added the Alaska Albers Equal Area projection to the standard
    projections defined in define_projections.R

--- R/define_projections.R
+++ R/define_projections.R
@@ -6,9 +6,17 @@
 #' @export
 
 define_projections <- function(){
-  # Cylindrical Equal Area projection
-  eaCRS <<- CRS('+proj=cea +lon_0=Central Meridian +lat_ts=Standard Parallel
-               +x_0=False Easting +y_0=False Northing')
-  # Decimal Latitude and Longitude
-  llCRS <<- CRS('+proj=longlat +ellps=WGS84')
+    # See http://www.spatialreference.org/
+    # Alaska Albers Equal Area Conic
+    # http://www.spatialreference.org/ref/esri/102006/
+    akCRS <<- CRS("+proj=aea 
+                   +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 
+                   +x_0=0 +y_0=0 
+                   +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+    # Cylindrical Equal Area projection
+    eaCRS <<- CRS("+proj=cea 
+                  +lon_0=Central Meridian +lat_ts=Standard Parallel
+                  +x_0=False Easting +y_0=False Northing")
+    # Decimal Latitude and Longitude
+    llCRS <<- CRS("+proj=longlat +ellps=WGS84")
 }
