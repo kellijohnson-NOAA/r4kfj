20b3bb18b6e7423499a93cf18b86bb447219153a
kellijohnson <kellifayejohnson@gmail.com>
2014-07-30 14:04:25 -0700

    Change akCRS projection from m to km

--- R/define_projections.R
+++ R/define_projections.R
@@ -12,7 +12,7 @@ define_projections <- function(){
     akCRS <<- CRS("+proj=aea 
                    +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 
                    +x_0=0 +y_0=0 
-                   +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+                   +ellps=GRS80 +datum=NAD83 +units=km +no_defs")
     # Cylindrical Equal Area projection
     eaCRS <<- CRS("+proj=cea 
                   +lon_0=Central Meridian +lat_ts=Standard Parallel
