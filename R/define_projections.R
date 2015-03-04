11e7795279ba8e06a30e71d5622f2d8df6a52e87
kellijohnson <kellifayejohnson@gmail.com>
2015-03-04 04:56:18 -0800

    spacing issues and new line at eof

--- R/define_projections.R
+++ R/define_projections.R
@@ -9,12 +9,12 @@ define_projections <- function(){
     # See http://www.spatialreference.org/
     # Alaska Albers Equal Area Conic
     # http://www.spatialreference.org/ref/esri/102006/
-    akCRS <<- CRS("+proj=aea 
-                   +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 
-                   +x_0=0 +y_0=0 
+    akCRS <<- CRS("+proj=aea
+                   +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154
+                   +x_0=0 +y_0=0
                    +ellps=GRS80 +datum=NAD83 +units=km +no_defs")
     # Cylindrical Equal Area projection
-    eaCRS <<- CRS("+proj=cea 
+    eaCRS <<- CRS("+proj=cea
                   +lon_0=Central Meridian +lat_ts=Standard Parallel
                   +x_0=False Easting +y_0=False Northing")
     # Decimal Latitude and Longitude
