0025e3eabcc0272075e3a1adc5d68b86f34b88f5
kellijohnson <kellifayejohnson@gmail.com>
2015-03-21 07:39:29 -0700

    Update load_packages
    Better code with less complication and more checks

--- R/load_packages.R
+++ R/load_packages.R
@@ -2,8 +2,8 @@
 #'
 #' Using \code{install.packages} and \code{require} a character
 #' vector of packages is i) checked if packages is already installed,
-#' ii) unistalled packages are installed, and iii) packages are 
-#' loaded. 
+#' ii) unistalled packages are installed, and iii) packages are
+#' loaded.
 #' @author Kelli Faye Johnson
 #' @param mypkg A character vector of package names.
 #' @param queitly A logical argument, ditctating if packages
@@ -11,20 +11,15 @@
 #' @export
 
 load_packages <- function(mypkg, install.quiet = TRUE) {
-  current.packages <- installed.packages()[, 1]
-  results <- which(is.element(mypkg, current.packages) == FALSE)
-  install.me <- mypkg[results]
+  if (!all(is.character(mypkg))) {
+    mypkg <- sapply(mypkg, as.character)
+  }
+  current.packages <- installed.packages()[, "Package"]
+  install.me <- mypkg[!mypkg %in% current.packages]
   if(length(install.me) > 0){
-    local({r <- getOption("repos"); 
-          r["CRAN"] <- "http://cran.stat.sfu.ca"; 
-          options(repos = r)})
-    install.packages(install.me, quiet = install.quiet)
+    install.packages(install.me, repos = "http://cran.stat.sfu.ca",
+      quiet = install.quiet)
   }
-  if(install.quiet == TRUE){
-  	quietly.warn <- FALSE
-  }
-  print.me <- lapply(mypkg, require, character.only = TRUE, 
-                     quietly = install.quiet, warn.conflicts = quietly.warn)
-  if(install.quiet == FALSE) return(unlist(print.me))
+  print.me <- lapply(mypkg, library, character.only = TRUE,
+    quietly = install.quiet, warn.conflicts = !install.quiet)
 }
-
