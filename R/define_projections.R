08325b7bb4592c41fcf01336f68faafc428571d6
kellijohnson <kellifayejohnson@gmail.com>
2016-11-18 08:06:25 -0800

    Update define_projections - declare CRS:: and document.
    import was not declared properly, now I use the :: and documentation
    was increased such that others might understand what is going on.
    The objects are still assigned to the global environment, and in the
    future it might be better to assign them to an environment and then
    export just that environment, or provide an additional argument where
    people could specify  the environment that they want.

--- R/define_projections.R
+++ R/define_projections.R
@@ -1,8 +1,14 @@
-#' Define equal area and decimal lat/lon projections
+#' Define equal projections and globally assign them.
 #'
-#' Defind the projections for i) cylindrical equal area and
-#' ii) decimal latitude and longitude.
-#' importFrom sp CRS
+#' Defind the projections for
+#'   \enumerate{
+#'     \item Alaska Albers
+#'     \item cylindrical equal area
+#'     \item decimal latitude and longitude.
+#'   }
+#' See \href{http://www.spatialreference.org/}{Spatial Reference}
+#'
+#' @importFrom sp CRS
 #' @author Kelli Faye Johnson
 #' @export
 
@@ -10,14 +16,14 @@ define_projections <- function(){
     # See http://www.spatialreference.org/
     # Alaska Albers Equal Area Conic
     # http://www.spatialreference.org/ref/esri/102006/
-    akCRS <<- CRS("+proj=aea
+    akCRS <<- sp::CRS("+proj=aea
                    +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154
                    +x_0=0 +y_0=0
                    +ellps=GRS80 +datum=NAD83 +units=km +no_defs")
     # Cylindrical Equal Area projection
-    eaCRS <<- CRS("+proj=cea
+    eaCRS <<- sp::CRS("+proj=cea
                   +lon_0=Central Meridian +lat_ts=Standard Parallel
                   +x_0=False Easting +y_0=False Northing")
     # Decimal Latitude and Longitude
-    llCRS <<- CRS("+proj=longlat +ellps=WGS84")
+    llCRS <<- sp::CRS("+proj=longlat +ellps=WGS84")
 }
