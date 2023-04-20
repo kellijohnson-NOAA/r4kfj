60cfc2c8e4510640c62a4ac8e13ba895f79a9110
Kelli Johnson <kelli.johnson@noaa.gov>
2023-04-20 07:56:11 -0700

    Formats [knitr_clean()]

--- R/knitr_clean.R
+++ R/knitr_clean.R
@@ -5,13 +5,15 @@
 #' @param workingdir specifiy the working directory
 #' @export
 
-    knitr_clean <- function(workingdir = NA) {
-        if(!is.na(workingdir)) {
-            useThisDir <- workingdir
-        } else {
-            useThisDir <- getwd()
-        }
-        system(paste0("cd '", useThisDir, "'",
-                      ";rm *.gz;rm *.toc;rm *.tex;rm *.log"))
-    }
+knitr_clean <- function(workingdir = NA) {
+  if(!is.na(workingdir)) {
+    useThisDir <- workingdir
+  } else {
+    useThisDir <- getwd()
+  }
+  system(paste0(
+    "cd '", useThisDir, "'",
+    ";rm *.gz;rm *.toc;rm *.tex;rm *.log"
+  ))
+}
 
